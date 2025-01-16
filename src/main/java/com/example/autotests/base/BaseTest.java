53c5014ccc4c506f6e66ba898110d45bd760a431
K <ktokar06@mail.ru>
2025-01-16 20:29:11 +0300

    Update

--- src/main/java/com/example/autotests/base/BaseTest.java
+++ src/main/java/com/example/autotests/base/BaseTest.java
@@ -1,11 +1,7 @@
 package com.example.autotests.base;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -14,7 +10,6 @@ import java.time.Duration;
 
 public class BaseTest {
     protected WebDriver driver;
-    protected WebDriverWait wait;
 
     @BeforeMethod
     public void setUp(){
