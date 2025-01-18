3aa9860bea32258684d05dc1c5d8ba91a649b1d4
K <103377916+ktokar06@users.noreply.github.com>
2025-01-19 00:24:03 +0300

    Update HomePage.java

--- src/test/java/com/example/autotests/pages/HomePage.java
+++ src/test/java/com/example/autotests/pages/HomePage.java
@@ -3,7 +3,6 @@ package com.example.autotests.pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
 /**
  * Страница главной страницы веб-приложения.
