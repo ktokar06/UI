b8a7bf17c79201595c5af32436594dc725b4ca9b
K <103377916+ktokar06@users.noreply.github.com>
2025-01-19 00:11:07 +0300

    Update HomePage.java

--- src/test/java/com/example/autotests/pages/HomePage.java
+++ src/test/java/com/example/autotests/pages/HomePage.java
@@ -4,7 +4,6 @@ import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
 /**
  * Страница главной страницы веб-приложения.
