7ecbbe8880f20a0cde65472d139d02ca1c46d069
K <ktokar06@mail.ru>
2025-02-01 17:39:18 +0300

    Update Commit

---
+++ hub.sh
@@ -0,0 +1,4 @@
+title hub
+TIMEOUT /T 3
+java -jar %SSS% -role hub
+TIMEOUT /T 15
---
+++ node.sh
@@ -0,0 +1,3 @@
+title node
+TIMEOUT /T 5
+java -Dwebdriver.chrome.driver=%THIS_FOLDER_PATH%/src/%CHROME_DRIVER_FILE_NAME% -jar %SSS% -role webdriver -hub %IP%:%WD_PORT%/grid/register -port %PORT%
