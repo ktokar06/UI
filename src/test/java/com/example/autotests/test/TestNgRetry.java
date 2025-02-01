05cdb77e72f41784176332bc13dcad4d3afe2ac3
K <ktokar06@mail.ru>
2025-02-01 19:39:24 +0300

    Update Commit

--- src/test/java/com/example/autotests/test/TestNgRetry.java
+++ src/test/java/com/example/autotests/test/TestNgRetry.java
@@ -3,21 +3,37 @@ package com.example.autotests.test;
 import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
 
+/**
+ * Реализация интерфейса {@link IRetryAnalyzer}, которая определяет стратегию повтора тестов в случае их провала.
+ * Этот класс используется для автоматического перезапуска тестов, которые потерпели неудачу, до достижения максимального количества попыток.
+ */
 public class TestNgRetry implements IRetryAnalyzer {
 
+    /**
+     * Текущий счетчик попыток перезапуска теста.
+     */
     private int count = 0;
+
+    /**
+     * Максимальное количество попыток перезапуска теста.
+     */
     private int maxCount = 2;
 
+    /**
+     * Метод, определяющий, нужно ли повторить выполнение теста после его провала.
+     *
+     * @param result Результат выполнения теста.
+     * @return {@code true}, если необходимо повторить выполнение теста, иначе {@code false}.
+     */
     @Override
     public boolean retry(ITestResult result) {
-        if(!result.isSuccess()){
+        if (!result.isSuccess()) {
             System.out.println("Тест упал! Попытка №" + (count + 1));
-            if(count < maxCount){
+            if (count < maxCount) {
                 count++;
                 return true;
             }
         }
         return false;
     }
-
 }
