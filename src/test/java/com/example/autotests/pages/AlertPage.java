c6b4948b12a603d2ee8b251f28858cea0d55d68f
K <ktokar06@mail.ru>
2025-02-04 17:02:11 +0300

    Update Commit

--- src/test/java/com/example/autotests/pages/AlertPage.java
+++ src/test/java/com/example/autotests/pages/AlertPage.java
@@ -1,20 +1,23 @@
 package com.example.autotests.pages;
 
+import com.example.autotests.util.WaitUtils;
 import io.qameta.allure.Step;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-
-import java.time.Duration;
 
 /**
  * Страница, содержащая элементы для работы с алертами.
  */
 public class AlertPage extends BasePage{
+
+    /**
+     * Локатор для фрейма, содержащего элементы для перетаскивания.
+     */
+    @FindBy(css = "iframe[src='alert/input-alert.html']")
+    private WebElement frameElement;
 
     /**
      * Переход на страницу InputAlert
@@ -25,14 +28,8 @@ public class AlertPage extends BasePage{
     /**
      * Кнопка для открытия алерт-бокса.
      */
-    @FindBy(xpath = "/html/body/button")
+    @FindBy(css = "body > button")
     private WebElement buttonAlertBox;
-
-    /**
-     * Элемент для вывода текста.
-     */
-    @FindBy(id = "demo")
-    private WebElement text;
 
     /**
      * Конструктор страницы, инициализирующий элементы страницы.
@@ -54,16 +51,6 @@ public class AlertPage extends BasePage{
     }
 
     /**
-     * Метод для установки элемента кнопки ввода алерта.
-     *
-     * @param inputButtonAlert элемент кнопки ввода алерта
-     */
-    @Step("Установить элемент кнопки ввода алерта")
-    public void setInputButtonAlert(WebElement inputButtonAlert) {
-        this.inputButtonAlert = inputButtonAlert;
-    }
-
-    /**
      * Метод для получения элемента кнопки открытия алерт-бокса.
      *
      * @return элемент кнопки открытия алерт-бокса
@@ -73,35 +60,6 @@ public class AlertPage extends BasePage{
         return buttonAlertBox;
     }
 
-    /**
-     * Метод для установки элемента кнопки открытия алерт-бокса.
-     *
-     * @param buttonAlertBox элемент кнопки открытия алерт-бокса
-     */
-    @Step("Установить элемент кнопки открытия алерт-бокса")
-    public void setButtonAlertBox(WebElement buttonAlertBox) {
-        this.buttonAlertBox = buttonAlertBox;
-    }
-
-    /**
-     * Метод для получения текста алерта.
-     *
-     * @return текст алерта
-     */
-    @Step("Получить текст алерта")
-    public WebElement getText() {
-        return text;
-    }
-
-    /**
-     * Метод для установки текста алерта.
-     *
-     * @param text текст алерта
-     */
-    @Step("Установить текст алерта")
-    public void setText(WebElement text) {
-        this.text = text;
-    }
 
     /**
      * Шаг для перехода на кнопку алерта.
@@ -116,13 +74,11 @@ public class AlertPage extends BasePage{
      */
     @Step("Открыть алерт-бокс")
     public void openAlertBox() {
-        WebElement frameElement = getDriver().findElement(By.cssSelector("iframe[src='alert/input-alert.html']"));
         getDriver().switchTo().frame(frameElement);
 
         buttonAlertBox.click();
 
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
-        Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+        Alert alert = getDriver().switchTo().alert();
         alert.sendKeys("Selenium Test!");
         alert.accept();
 
@@ -133,13 +89,12 @@ public class AlertPage extends BasePage{
      * Шаг для проверки наличия текста алерта.
      */
     @Step("Проверить наличие текста алерта")
-    public void isAlertTextPresent() {
-        WebElement frameElement = getDriver().findElement(By.cssSelector("iframe[src='alert/input-alert.html']"));
+    public boolean isAlertTextPresent() {
         getDriver().switchTo().frame(frameElement);
 
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.visibilityOf(text)).isDisplayed();
+        boolean alertText = WaitUtils.waitForElementPresence(getDriver(), By.id("demo"));
 
         getDriver().switchTo().defaultContent();
+        return alertText;
     }
 }
