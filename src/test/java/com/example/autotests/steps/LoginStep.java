8ec902ff92cd8e29dbe7084aa68ad94e812778fc
K <ktokar06@mail.ru>
2025-02-08 00:44:46 +0300

    Update Commit

--- src/test/java/com/example/autotests/steps/LoginStep.java
+++ src/test/java/com/example/autotests/steps/LoginStep.java
@@ -1,11 +1,13 @@
 package com.example.autotests.steps;
 
+import com.example.autotests.pages.LoginPage;
 import com.example.autotests.test.BaseTest;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-import static com.example.autotests.config.MyConfig.URL_LOGIN;
+import static com.example.autotests.config.MyConfig.*;
+import static com.example.autotests.config.MyConfig.VALID_USERNAME_DESCRIPTION;
 
 public class LoginStep extends BaseTest {
 
@@ -16,11 +18,21 @@ public class LoginStep extends BaseTest {
 
     @When("Я ввожу имя пользователя и пароль")
     public void enterCredentials(String credentialsType) {
+        LoginPage loginPage = new LoginPage(getDriver());
+
+        loginPage.setUsername(VALID_USERNAME)
+                .setPassword(VALID_PASSWORD)
+                .setUsernameDescription(VALID_USERNAME_DESCRIPTION)
+                .setClickLoginButton();
+
 
     }
+
     @When("Я нажимаю на кнопку Войти")
     public void clickButton(String buttonText) {
+        LoginPage loginPage = new LoginPage(getDriver());
 
+        loginPage.setClickLoginButton();
     }
 
     @Then("Я должен увидеть сообщение")
