85caaf5dac7ddacb26cee6958afbae406347aa3c
K <ktokar06@mail.ru>
2025-02-08 21:38:38 +0300

    Update Commit

--- src/test/java/com/example/autotests/steps/LoginStep.java
+++ src/test/java/com/example/autotests/steps/LoginStep.java
@@ -6,12 +6,38 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+
 import static com.example.autotests.config.MyConfig.*;
 
 public class LoginStep {
 
     private WebDriver driver;
+    /**
+     * Поле для ввода имени пользователя.
+     */
+    @FindBy(id = "username")
+    private WebElement username;
+
+    /**
+     * Поле для ввода пароля.
+     */
+    @FindBy(id = "password")
+    private WebElement password;
+
+    /**
+     * Поле для ввода описания имени пользователя.
+     */
+    @FindBy(id = "formly_1_input_username_0")
+    private WebElement usernameDescription;
+
+    /**
+     * Кнопка для отправки формы входа.
+     */
+    @FindBy(className = "btn-danger")
+    private WebElement submitButton;
 
     @Given("I am on the login page")
     public void i_am_on_the_login_page() {
@@ -29,11 +55,11 @@ public class LoginStep {
 
     @When("I click on the {string} button")
     public void i_click_on_the_button(String buttonText) {
-        driver.findElement(By.className("btn-danger")).click();
+        driver.findElement(By.cssSelector("btn-danger")).click();
     }
 
     @Then("I should see the message {string}")
     public boolean i_should_see_the_message(String expectedMessage) {
-        return WaitUtils.waitForElementPresence(driver, By.cssSelector(".ng-scope"));
+        return WaitUtils.waitForElementPresence(driver, By.cssSelector(".ng-scope "));
     }
 }
--- src/test/java/com/example/autotests/test/RunTest.java
+++ src/test/java/com/example/autotests/test/RunTest.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features/alerts.feature",
+        features = "src/test/resources/features/login.feature",
         glue = "com.example.autotests.steps"
 )
 public class RunTest{
