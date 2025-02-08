51dc97803f50a5456c12d7ac7664a8f37d0fa809
K <ktokar06@mail.ru>
2025-02-08 03:59:08 +0300

    Update Commit

--- src/test/java/com/example/autotests/steps/LoginStep.java
+++ src/test/java/com/example/autotests/steps/LoginStep.java
@@ -1,8 +1,10 @@
 package com.example.autotests.steps;
 
+import com.example.autotests.util.WaitUtils;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import static com.example.autotests.config.MyConfig.*;
@@ -12,24 +14,24 @@ public class LoginStep {
     private WebDriver driver;
 
     @Given("I am on the login page")
-    public void i_am_on_the_login_page(){
+    public void i_am_on_the_login_page() {
         driver = new ChromeDriver();
         driver.get(URL_LOGIN);
-
     }
 
     @When("I enter valid username and password")
     public void i_enter_valid_username_and_password() {
-
+        driver.findElement(By.id("username")).sendKeys(USERNAME);
+        driver.findElement(By.id("password")).sendKeys(PASSWORD);
     }
-
     @When("I click on the {string} button")
-    public void i_click_on_the_button(String b) {
+    public void i_click_on_the_button(String buttonText) {
+        driver.findElement(By.className("btn-danger")).click();
 
     }
-
     @Then("I should see the message {string}")
-    public void i_should_see_the_message(String string) {
-
+    public boolean i_should_see_the_message(String expectedMessage) {
+        return WaitUtils.waitForElementPresence(driver, By.cssSelector(".ng-scope"));
     }
+
 }
