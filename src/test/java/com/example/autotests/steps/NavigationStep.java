1c2fb884b8842116b5ef230c60c028d5cf8e2c9a
K <ktokar06@mail.ru>
2025-02-08 03:46:17 +0300

    Update Commit

--- src/test/java/com/example/autotests/steps/NavigationStep.java
+++ src/test/java/com/example/autotests/steps/NavigationStep.java
@@ -1,17 +1,5 @@
 package com.example.autotests.steps;
 
-import com.example.autotests.pages.BasePage;
-import com.example.autotests.test.BaseTest;
-import org.openqa.selenium.WebDriver;
+public class NavigationStep  {
 
-public class NavigationStep extends BasePage {
-
-    /**
-     * Конструктор страницы, инициализирующий элементы страницы.
-     *
-     * @param driver Экземпляр веб-драйвера, необходимый для взаимодействия со страницей.
-     */
-    public NavigationStep(WebDriver driver) {
-        super(driver);
-    }
 }
