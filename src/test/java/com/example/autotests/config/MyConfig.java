3e75105ae4c363c4456a28fbe9dcae498959be04
K <103377916+ktokar06@users.noreply.github.com>
2025-05-28 05:32:21 +0300

    Update MyConfig.java

--- src/test/java/com/example/autotests/config/MyConfig.java
+++ src/test/java/com/example/autotests/config/MyConfig.java
@@ -5,88 +5,23 @@ package com.example.autotests.config;
  */
 public class MyConfig {
 
-    /**
-     * Cookie URL страницы входа.
-     */
-    public static final String URL_COOKIE = "https://www.sql-ex.ru/";
+    public static final String URL_COOKIE = "url_site";
+    public static final  String URL_ALERT = "url_site";
+    public static final String URL_BASIC = "url_site";
+    public static final String URL_TABS = "url_site";
+    public static final String URL_IFRAME = "url_site";
+    public static final String URL_LOGIN = "url_site";
+    public final static String URL_TITLE = "url_site";
 
-     /**
-     * URL страницы с алертами.
-     */
-    public static final  String URL_ALERT = "http://way2automation.com/way2auto_jquery/alert.php";
-
-    /**
-     * Базовый URL для галереи аутентификации HTTPWatch.
-     */
-    public static final String URL_BASIC = "https://www.httpwatch.com/httpgallery/authentication/#showExample10";
-
-     /**
-     * URL TABS.
-     */
-    public static final String URL_TABS = "http://way2automation.com/way2auto_jquery/frames-and-windows.php";
-
-    /**
-     * URL IFRAME.
-     */
-    public static final String URL_IFRAME = "http://way2automation.com/way2auto_jquery/droppable.php";
-
-    /**
-     * URL страницы входа.
-     */
-    public static final String URL_LOGIN = "https://www.way2automation.com/angularjs-protractor/registeration/#/login";
-
-    /**
-     * Основной URL сайта.
-     */
-    public final static String URL_TITLE = "https://www.way2automation.com/";
-
-    /**
-     * Валидный логин пользователя.
-     */
-    public static final String VALID_USERNAME = "angular";
-
-    /**
-     * Валидный пароль пользователя.
-     */
+    
+    public static final String VALID_USERNAME = "name";
     public static final String VALID_PASSWORD = "password";
-
-    /**
-     * Описание валидного пользователя.
-     */
-    public static final String VALID_USERNAME_DESCRIPTION = "hello";
-
-    /**
-     * Невалидный логин пользователя.
-     */
-    public static final String INVALID_USERNAME = "user";
-
-    /**
-     * Невалидный пароль пользователя.
-     */
-    public static final String INVALID_PASSWORD = "pass24word";
-
-    /**
-     * Описание невалидного пользователя.
-     */
+    public static final String VALID_USERNAME_DESCRIPTION = "name";
+    public static final String INVALID_USERNAME = "name";
+    public static final String INVALID_PASSWORD = "password";
     public static final String INVALID_USERNAME_DESCRIPTION = "description";
-
-    /**
-     * Валидное имя пользователя для входа.
-     */
-    public static final String COOKIE_VALID_USERNAME = "zegwaer";
-
-    /**
-     * Валидный пароль пользователя для входа.
-     */
-    public static final String COOKIE_VALID_PASSWORD = "123456789";
-  
-     /**
-     * Имя пользователя для авторизации.
-     */
-    public static final String USERNAME = "httpwatch";
-
-    /**
-     * Пароль для авторизации.
-     */
-    public static final String PASSWORD = "httpwatch";
+    public static final String COOKIE_VALID_USERNAME = "name";
+    public static final String COOKIE_VALID_PASSWORD = "password";
+    public static final String USERNAME = "name";
+    public static final String PASSWORD = "password";
 }
